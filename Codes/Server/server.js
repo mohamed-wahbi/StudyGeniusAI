91eeb14673779ca48239e267e4e6503440d05f23
mohamed-wahbi <mohamedwahbimohamedwahbi@gmail.com>
2024-07-19 02:46:27 +0200

    ajouter cree et connecte avec une base de donée non sql 'StudyGenius' .

--- Codes/Server/server.js
+++ Codes/Server/server.js
@@ -1,5 +1,7 @@
 const express = require ("express");
 const app = express();
+//connecting with DB :
+require('./Config/connect.js')
 
 const PORT = process.env.PORT || 4040
 app.listen(4040,()=>console.log(`Server is connected on PORT = ${PORT}`))
