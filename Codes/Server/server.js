23288327ac27f96e0cdbab0df6cc083e5424860b
mohamed-wahbi <mohamedwahbimohamedwahbi@gmail.com>
2024-07-19 02:50:33 +0200

    installer 'cors' et 'dotenv' puis demare le middlwear cors dans notre projet .

--- Codes/Server/server.js
+++ Codes/Server/server.js
@@ -3,5 +3,16 @@ const app = express();
 //connecting with DB :
 require('./Config/connect.js')
 
+const cors = require('cors');
+
+
+
+//middlwears :
+app.use(cors())
+app.use(express.json());
+
+
+
+
 const PORT = process.env.PORT || 4040
 app.listen(4040,()=>console.log(`Server is connected on PORT = ${PORT}`))
